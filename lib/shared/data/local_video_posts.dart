c80abc3f7246afd6334c950775a6cadf7e42437d
Darwin <darwinpatricio_8@hotmail.com>
2024-10-18 11:54:00 +0200

    First commit

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,15 @@
+
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'Que borroso veo todo!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 21564320,
+    'views': 123563,
+  },
+  {
+    'name': '¿Esto es trigo? que interesante',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 320,
+    'views': 2300,
+  }
+];
